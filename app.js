72427a317248d50e5ca9901ef2864877fb9cd9a7
guidoasbun <gdrodrigo8@gmail.com>
2020-02-21 00:05:47 -0800

    created html templates

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const fs = require('fs')
+const prompt = require('inquirer').createPromptModule()
+const path = require('path')
+
 const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
-const employee = new Employee('Rocky', 1234,'manager')
-
-employee.getRole()
